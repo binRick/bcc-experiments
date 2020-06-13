e6e2b3fd636eb09137ec51e15f612cafeeca3c30
root <admin@vpntech.net>
2020-06-13 19:51:13 -0400

    tweaks

--- tcpconnect.sh
+++ tcpconnect.sh
@@ -2,9 +2,9 @@
 set -e
 
 if cat /etc/redhat-release | grep 'release 7'; then
-	cmd="exec command python2 ./_execsnoop.py"
+	cmd="exec command python2 ./_tcpconnect.py"
 else
-	cmd="exec command python ./_execsnoop.py"
+	cmd="exec command python ./_tcpconnect.py"
 fi
 
 >&2 echo -e $cmd
