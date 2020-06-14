2f5325d689efa77b5c02324480dbbcfccad4c5e0
root <admin@vpntech.net>
2020-06-13 21:32:24 -0400

    proof of concept to dump bash vars

---
+++ sqlsnooper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+#source setup.sh
+
+if cat /etc/redhat-release | grep 'release 7'; then
+	cmd="command sudo -nu root command python2 ./_sqlsnooper.py"
+else
+	cmd="commnd sudo -nu root command /usr/libexec/platform-python ./_sqlsnooper.py"
+fi
+
+
+cmd="exec $cmd $(pgrep -n mysqld) 0"
+>&2 echo -e $cmd
+
+
+eval $cmd
