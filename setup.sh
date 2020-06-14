268b3beec208bac071641cd0ea359753a0547fa5
root <root@vpn299.vpnservice.company>
2020-06-13 21:14:38 -0400

    working in $(cat /etc/redhat-release)

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 command -v nodemon >/dev/null || npm i nodemon -g
 [[ ! -e .v/bin/activate ]] && python3 -m venv .v
 source .v/bin/activate
+pip install simplejson
 #pip install psutil bcc loguru
--- tail_supervisor_logs.sh
+++ tail_supervisor_logs.sh
@@ -1,1 +1,1 @@
-tail -F service/*/log/main/current
+while :; do timeout 30 reap -vx tail -q -n0 -F service/*/log/main/current || sleep 5; sleep .01; done
