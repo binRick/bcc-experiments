d9acf24b81823ade59486f317b8f22f25b4ef948
root <root@vpn299.vpnservice.company>
2020-06-13 21:16:03 -0400

    working in $(cat /etc/redhat-release)

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 command -v nodemon >/dev/null || npm i nodemon -g
 [[ ! -e .v/bin/activate ]] && python3 -m venv .v
+pip3 install simplejson
 source .v/bin/activate
 pip install simplejson
 #pip install psutil bcc loguru
