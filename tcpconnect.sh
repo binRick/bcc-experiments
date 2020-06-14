a88fc1f2a67e69d3b3d938185bd6d0a09f361748
root <admin@vpntech.net>
2020-06-14 04:13:41 -0400

    tweaks

--- tcpconnect.sh
+++ tcpconnect.sh
@@ -4,9 +4,9 @@ cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #source setup.sh
 
 if cat /etc/redhat-release | grep 'release 7'; then
-	cmd="exec command python2 ./_tcpconnect.py"
+	cmd="exec command reap -x python2 ./_tcpconnect.py"
 else
-	cmd="exec command /usr/libexec/platform-python ./_tcpconnect.py"
+	cmd="exec command reap -x /usr/libexec/platform-python ./_tcpconnect.py"
 fi
 
 >&2 echo -e $cmd
