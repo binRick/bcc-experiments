7a83639e49c5dd7ca0fac44e25ac9b97fefe9bbd
Richard Blundell <rblundell@deloitte.com>
2020-06-15 10:14:14 -0400

    ensure sudo

--- execsnoop.sh
+++ execsnoop.sh
@@ -4,10 +4,10 @@ cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #source setup.sh
 
 if cat /etc/redhat-release | grep 'release 7'; then
-	cmd="exec command python2 ./_execsnoop.py"
+	cmd="command python2 ./_execsnoop.py"
 else
-	cmd="exec command /usr/libexec/platform-python ./_execsnoop.py"
+	cmd="command /usr/libexec/platform-python ./_execsnoop.py"
 fi
-
+cmd="exec sudo $cmd"
 >&2 echo -e $cmd
 eval $cmd
